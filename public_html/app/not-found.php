3938ba90a75021c686a94ca48b02fe467e16d9db
pvars <0796554628pvars>
2021-06-22 13:49:56 +0700

    add file not-found.php -v2

---
+++ public_html/app/not-found.php
@@ -0,0 +1,91 @@
+<?php
+
+//Common setting
+require_once ('config.php');
+require_once ('lib.php');
+
+session_start();
+
+//-----------------------------------------------------------
+// HTML
+//-----------------------------------------------------------
+$titleHTML = '';
+$cssHTML = '';
+$scriptHTML = '';
+
+echo <<<EOF
+<!DOCTYPE html>
+<html>
+<head>
+EOF;
+
+//Meta CSS
+include ($TEMP_APP_META_PATH);
+
+echo <<<EOF
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+EOF;
+
+//Preloader
+//include ($TEMP_APP_PRELOADER_PATH);
+
+//Header
+include ($TEMP_APP_HEADER_PATH);
+
+//Menu
+include ($TEMP_APP_MENU_MOD_PATH);
+
+//Conntent
+echo <<<EOF
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>404 Không tìm thấy</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="home.php">Trang chủ</a></li>
+              <li class="breadcrumb-item active">Không tìm thấy</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="error-page">
+        <h2 class="headline text-warning"> 404</h2>
+
+        <div class="error-content">
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Không tìm thấy trang.</h3>
+
+          <p>
+            Trang bạn tìm hiện không có.
+            Hãy <a href="home.php">trở lại trang chủ</a>.
+          </p>
+        </div>
+        <!-- /.error-content -->
+      </div>
+      <!-- /.error-page -->
+    </section>
+    <!-- /.content -->
+  </div>
+EOF;
+
+//Footer
+include ($TEMP_APP_FOOTER_PATH);
+//Meta JS
+include ($TEMP_APP_METAJS_PATH);
+echo <<<EOF
+    </div>
+</body>
+</html>
+EOF;
+
+?>
